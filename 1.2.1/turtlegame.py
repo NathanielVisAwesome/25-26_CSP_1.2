44d47efc1130b0814fd864d70bf1888782b35cbb
VermetraSchool <natevitale10@gmail.com>
2025-12-04 13:44:29 -0500

    alalalalala :3

--- 1.2.1/turtlegame.py
+++ 1.2.1/turtlegame.py
@@ -9,23 +9,26 @@ creator = "Nathaniel M. Vitale"
 schoolclass = "Computer Science Principals AP"
 teacher = "A. Kiedes"
 
-
 # Initialize screen
 
 wn = trtl.Screen()
-wn.register_shape("tolkie-cool.gif")
+print("Game by:",creator,"In",teacher+"'s",schoolclass,"class")
+
 
 # Game configuration
 
 spot_color = "red"
 clicked_color = "dark red"
+score = 0
 
 
 # Initialize turtle as "Game"
 
 game = trtl.Turtle()
-game.shape("tolkie-cool.gif")
+game.shape("circle")
 
+# Score Writer
+score_writer = trtl.Turtle()
 
 # Turtle features
 
@@ -36,13 +39,27 @@ game.shapesize(5)
 
 # Game functions
 
+# Score Maker
+def update_score():
+    global score
+    score += 1
+    print("Score:",score)
+
+# Change turtle position
+def change_pos():
+    newX = rand.randint(-300,300)
+    newY = rand.randint(-300,300)
+    game.goto(newX, newY)
+    update_score()
+
 #Get a score boost, change turtle position to a random coordinate
 def click(x, y):
-    print("Turtle has been Clicked! Game by:",creator, schoolclass,teacher)
     game.color(clicked_color)
     game.color(spot_color)
-    rand
+    change_pos()
 
+def score_update():
+    print("Scorewriter is not ready at this time")
 
 # Game Events
 
